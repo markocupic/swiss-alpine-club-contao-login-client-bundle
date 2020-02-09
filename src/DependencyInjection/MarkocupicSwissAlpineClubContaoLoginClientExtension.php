f941b184940f8b5c541f0cc7927965839349cefd
Marko Cupic <m.cupic@gmx.ch>
2020-02-09 10:40:31 +0100

    dev

--- src/DependencyInjection/MarkocupicSwissAlpineClubContaoLoginClientExtension.php
+++ src/DependencyInjection/MarkocupicSwissAlpineClubContaoLoginClientExtension.php
@@ -19,15 +19,43 @@ use Symfony\Component\Config\FileLocator;
 
 class MarkocupicSwissAlpineClubContaoLoginClientExtension extends Extension
 {
+
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfiguration(array $config, ContainerBuilder $container): Configuration
+    {
+        return new Configuration(
+            $container->getParameter('kernel.project_dir'),
+            $container->getParameter('kernel.default_locale')
+        );
+    }
+
     public function load(array $configs, ContainerBuilder $container)
     {
+
+        $configuration = new Configuration(
+            $container->getParameter('kernel.project_dir'),
+            $container->getParameter('kernel.default_locale')
+        );
+
+        $config = $this->processConfiguration($configuration, $configs);
+
         $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__ . '/../Resources/config')
         );
+
         $loader->load('parameters.yml');
         $loader->load('listener.yml');
         $loader->load('services.yml');
         $loader->load('controller-contao-frontend-module.yml');
+
+        $container->setParameter('markocupic.swiss_alpine_club_contao_login_client.session.attribute_bag_name', $config['swiss_alpine_club_contao_login_client']['session']['attribute_bag_name']);
+        $container->setParameter('markocupic.swiss_alpine_club_contao_login_client.session.attribute_bag_key', $config['swiss_alpine_club_contao_login_client']['session']['attribute_bag_key']);
+        $container->setParameter('markocupic.swiss_alpine_club_contao_login_client.session.flash_bag_key', $config['swiss_alpine_club_contao_login_client']['session']['flash_bag_key']);
+
     }
 }
