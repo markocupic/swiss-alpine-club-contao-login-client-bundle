a4f2f94c32d5b9fe4af38213a906531e92fabe95
Marko Cupic <m.cupic@gmx.ch>
2021-12-11 14:47:40 +0100

    css button line break fix

--- src/Session/Attribute/ArrayAttributeBag.php
+++ src/Session/Attribute/ArrayAttributeBag.php
@@ -21,6 +21,7 @@ use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBag;
  */
 class ArrayAttributeBag extends AttributeBag implements \ArrayAccess
 {
+
     public function offsetExists($offset): bool
     {
         return $this->has($offset);
