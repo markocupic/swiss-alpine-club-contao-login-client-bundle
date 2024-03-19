bbd37a467e191ccc6517547154deda6025f48622
Marko Cupic <m.cupic@gmx.ch>
2024-03-19 19:07:45 +0100

    Use controllers class name for the route name

--- src/Controller/GetLogoutEndpointController.php
+++ src/Controller/GetLogoutEndpointController.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of Swiss Alpine Club Contao Login Client Bundle.
  *
- * (c) Marko Cupic 2023 <m.cupic@gmx.ch>
+ * (c) Marko Cupic 2024 <m.cupic@gmx.ch>
  * @license MIT
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
@@ -18,7 +18,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/ssoauth/get_logout_endpoint', name: 'swiss_alpine_club_sso_login_get_logout_endpoint')]
+#[Route('/ssoauth/get_logout_endpoint', name: self::class)]
 class GetLogoutEndpointController extends AbstractController
 {
     public function __construct(
